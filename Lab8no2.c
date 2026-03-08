13a30048d6cb5fcfa7cc9579c6e4d93570f6ca90
6704062617369 <s6704062617369@email.kmutnb.ac.th>
2026-03-08 16:39:03 +0700

    show number of no.1 stat

--- Lab8no2.c
+++ Lab8no2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int hardestQuestion(char ans[][10], int n);
+int countNo1Correct(char ans[][10], int n);
 
 char keys[10] = {'D','B','D','C','C','D','A','E','A','D'};
 
@@ -17,33 +17,19 @@ int main() {
         {'E','B','E','C','C','D','E','E','A','D'}
     };
 
-    printf("Hardest question = No.%d\n",
-           hardestQuestion(ans, 8) + 1);
+    printf("No.1 correct = %d people\n", countNo1Correct(ans, 8));
 
     return 0;
 }
 
-int hardestQuestion(char ans[][10], int n) {
+int countNo1Correct(char ans[][10], int n) {
 
-    int correct[10] = {0};
-    int i, j;
+    int i, count = 0;
 
     for(i = 0; i < n; i++) {
-        for(j = 0; j < 10; j++) {
-            if(ans[i][j] == keys[j])
-                correct[j]++;
-        }
+        if(ans[i][0] == keys[0])
+            count++;
     }
 
-    int min = correct[0];
-    int idx = 0;
-
-    for(i = 1; i < 10; i++) {
-        if(correct[i] < min) {
-            min = correct[i];
-            idx = i;
-        }
-    }
-
-    return idx;
+    return count;
 }
